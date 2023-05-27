e2abffdf53efa82abff7329bba71acf2266a7710
suvash <champsuvash@gmail.com>
2023-05-27 19:52:05 +0545

    about sectioN

--- src/components/About.js
+++ src/components/About.js
@@ -9,15 +9,16 @@ function About() {
         <img src={profile} alt="sushi" id="profile" />
         <div className="hero-about">
           <div className="hero-name">
-            Hi im <span className="nickname">sushi</span>
+            hola soy <span className="nickname">sushi</span>
           </div>
           <section className="hero-info">
-            Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem
-            ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum Lorem
-            ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem
-            ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum Lorem ipsumLorem
-            ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem
-            ipsumLorem ipsumLorem ipsumLorem ipsum
+            I'm a front-end developer with a Bachelor's degree in Electronics
+            Engineering from IOE Pulchowk Campus. Specializing in
+            mobile-responsive web development, I create visually stunning and
+            highly functional websites that adapt seamlessly across platforms.
+            With a passion for technology and an unwavering commitment to
+            excellence, I'm ready to collaborate with you and bring your digital
+            vision to life. Let's connect and make something amazing together!
           </section>
         </div>
       </div>
