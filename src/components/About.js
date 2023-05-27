8b923819d9f55744996fa67665a58290e6e4694c
suvash <champsuvash@gmail.com>
2023-05-27 13:47:19 +0545

    about layout

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,19 @@
 import React from "react";
 import "../styles/About.css";
-import { Parallax, ParallaxLayer } from "@react-spring/parallax";
+import { motion } from "framer-motion";
+import iconX from "../images/alpha-x.svg";
+import iconO from "../images/alpha-o.svg";
 
 function About() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <main className="about">
+        <div className="main-text">
+          <div className="main-name">Suvash Joshi</div>
+          <div className="main-title">Front End Developer</div>
+        </div>
+      </main>
+    </>
   );
 }
 
