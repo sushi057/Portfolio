11bf99b8040452dd8e8361e0086efa3125d3b8ea
suvash <champsuvash@gmail.com>
2023-06-04 21:04:22 +0545

    projects data updated

--- src/assets/data/projectList.js
+++ src/assets/data/projectList.js
@@ -1,11 +1,14 @@
-import placeholder from "../images/sushi.png";
+import herdim from "../images/herdim-na.png";
+import sneaker from "../images/sneaker-point.png";
+import weathering from "../images/weathering.png";
+import waldo from "../images/wheres-waldo.png";
 
 const projectList = [
   {
     name: "Herdim na ta",
     live: "https://sushi057.github.io/herdim-na-ta/",
     github: "https://github.com/sushi057/herdim-na-ta",
-    image: placeholder,
+    image: herdim,
     description:
       "'Herdim Na Ta' is your one-stop destination for Nepali entertainment. This streaming app brings you a vast collection of new and trending TV shows, movies, and cartoons from Nepal and beyond. Stay up to date with the latest releases and immerse yourself in a diverse library of genres. With Herdim Na Ta, enjoy a seamless streaming experience and indulge in the magic of Nepali entertainment anytime, anywhere.",
   },
@@ -13,7 +16,7 @@ const projectList = [
     name: "Weathering with you",
     live: "https://sushi057.github.io/Weather-App/",
     github: "https://github.com/sushi057/Weather-App",
-    image: placeholder,
+    image: weathering,
     description:
       "Weathering with you is a sleek and user-friendly web app developed with React. It utilizes the OpenWeather API to provide real-time weather information based on your location. With just a glance, you can view the current temperature and humidity, helping you plan your day accordingly. Weatherify's responsive design ensures a seamless experience across devices, while its fast performance guarantees instant updates. Stay informed and prepared with Weatherify, your personalized weather companion.",
   },
@@ -21,7 +24,7 @@ const projectList = [
     name: "Wheres Waldo: Anime Edition",
     live: "https://sushi057.github.io/Wheres-Waldo/",
     github: "https://github.com/sushi057/Wheres-Waldo",
-    image: placeholder,
+    image: waldo,
     description:
       "Welcome to Sneaker Point, the go-to website for all your trendy footwear needs. Whether you're a man, woman, or shopping for kids, we've got you covered with an extensive collection of the latest and most sought-after shoes. From sleek sneakers to stylish boots and everything in between, Sneaker Point offers a variety of options to suit every taste and style. Discover the hottest trends, find the perfect fit, and step up your shoe game with Sneaker Point, your ultimate footwear destination.",
   },
@@ -29,7 +32,7 @@ const projectList = [
     name: "Sneaker Point",
     live: "https://sushi057.github.io/Shopping-Cart/",
     github: "https://github.com/sushi057/Shopping-Cart",
-    image: placeholder,
+    image: sneaker,
     description:
       "Discover the thrill of 'Wheres Waldo: Anime Edition!' This engaging web app challenges you to find listed anime characters within beautifully illustrated scenes. Race against the ticking clock as you sharpen your observation skills and unlock new levels of excitement. With stunning visuals, increasing difficulty, and addictive gameplay, Wheres Waldo: Anime Edition guarantees an immersive search adventure like no other. Embark on this unforgettable quest today and become a master of spotting your favorite anime characters",
   },
