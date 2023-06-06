76b76ba85a1e55788b51f5d81c507c54446f68f7
suvash <champsuvash@gmail.com>
2023-06-06 16:34:46 +0545

    blobs added

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,11 +1,19 @@
 import React from "react";
 import "../styles/Hero.css";
 import { motion } from "framer-motion";
+import blob1 from "../assets/images/blob-1.svg";
+import blob2 from "../assets/images/blob-2.svg";
+import blob3 from "../assets/images/blob-3.svg";
 
 function Hero() {
   return (
     <>
       <main className="about">
+        <section className="blobs">
+          <img src={blob1} alt="blob" className="blob-one" />
+          <img src={blob2} alt="blob" className="blob-two" />
+          <img src={blob3} alt="blob" className="blob-three" />
+        </section>
         <div className="main-text">
           <div id="main-name">SUVASH JOSHI</div>
           <div id="main-title">FRONT END DEVELOPER</div>
